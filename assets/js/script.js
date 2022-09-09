61187fb47279d9877abb4e1bb11f30d4dbf98d5f
Mpergolatti <Mpergolatti@gmail.com>
2022-09-09 14:08:50 -0400

    added preventDefault to stop the page from auto refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttonEl = document.querySelector("#save-task"); // button selector
+// var buttonEl = document.querySelector("#save-task"); // button selector
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); // UL selector
 
-var createTaskHandler = function() { // universal function for button click 
+var createTaskHandler = function(event) { // universal function for button click 
+  event.preventDefault();
+
   var listItemEl = document.createElement("li"); // creates the LI
     listItemEl.className = "task-item"; // Styles the element
     listItemEl.textContent = "This is a new task."; // creates text within the new task
     tasksToDoEl.appendChild(listItemEl); // appends the UL to the LI
-}
+};
 
-buttonEl.addEventListener("click", createTaskHandler); // calls the add a task function
+formEl.addEventListener("submit", createTaskHandler); // calls the add a task function
